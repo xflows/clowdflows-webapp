1793a4d6e8ac04cb96cfd1244ef8949fb0ac2fa2
Anze Vavpetic <anze.vavpetic@ijs.si>
2016-07-29 14:41:18 +0200

    Remove config.ts from version control; use _config.ts as a template (temp solution)

--- app/config.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.BASE_URL = 'http://127.0.0.1:8001';
-exports.API_ENDPOINT = exports.BASE_URL + '/api/';
-exports.TEST_TOKEN = '03f8dcf3d938b4388a1fc331328a49e6ff66fd36';
-//# sourceMappingURL=config.js.map
