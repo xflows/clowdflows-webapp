9ce43cea6af54d8f91a8c9958e970c80cc999b40
Anze Vavpetic <anze.vavpetic@ijs.si>
2016-12-16 12:18:35 +0100

    Scroll to bottom of logger console after a new message arrives

--- src/app/components/editor/logging/logging.component.ts
+++ src/app/components/editor/logging/logging.component.ts
@@ -1,27 +1,29 @@
-import {Component, ElementRef, ViewChild, Input, DoCheck, OnInit} from '@angular/core';
+import {
+    Component, ElementRef, ViewChild, Input, OnInit, AfterViewInit, ViewChildren, QueryList
+} from '@angular/core';
 
 @Component({
     selector: 'logging',
     template: require('./logging.component.html'),
     styles: [require('./logging.component.css'),]
 })
-export class LoggingComponent implements DoCheck, OnInit {
-
+export class LoggingComponent implements OnInit, AfterViewInit {
     @ViewChild('log') private logContainer:ElementRef;
+    @ViewChildren('msgEl') private messagesElements:QueryList<any>;
     @Input() messages:Array<any>;
     numberOfMessages:number = 0;
-
-    constructor() {}
 
     ngOnInit() {
         this.numberOfMessages = this.messages.length;
     }
 
-    ngDoCheck() {
-        if (this.numberOfMessages < this.messages.length) {
-            this.scrollToBottom();
-            this.numberOfMessages = this.messages.length;
-        }
+    ngAfterViewInit() {
+        this.messagesElements.changes.subscribe(() => {
+            if (this.numberOfMessages < this.messages.length) {
+                this.scrollToBottom();
+                this.numberOfMessages = this.messages.length;
+            }
+        });
     }
 
     scrollToBottom():void {
