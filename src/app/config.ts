011207af3d63ddb2177126392232b829683324be
Nika Eržen <nikae@genericno.ijs.si>
2020-02-19 12:09:58 +0100

    console log test https

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,3 +1,6 @@
 export const DOMAIN =       typeof process.env.API.domain == 'undefined' ? '127.0.0.1:8000' : process.env.API.domain;
 export const BASE_URL =     typeof process.env.API.baseUrl == 'undefined' ? 'http://' + DOMAIN : process.env.API.baseUrl;
 export const API_ENDPOINT = typeof process.env.API.apiEndpoint == 'undefined' ? BASE_URL + '/api/' : process.env.API.apiEndpoint;
+
+console.log(DOMAIN, BASE_URL, API_ENDPOINT)
+console.log(API_ENDPOINT.replace("http","https"))
